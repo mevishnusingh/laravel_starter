057fe534188beaae41232d0e732c492fa49d478d
vishnu.interbit <vishnu.interbitsolutions.com>
2022-07-10 14:52:48 +0530

    Spatie: creating role and assign to user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
@@ -15,11 +16,13 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::Create([
+        $user = User::Create([
             'name' => 'Admin',
             'email' => 'admin@admin.com',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ]);
+
+        $user->assignRole(['writer', 'admin']);
     }
 }
